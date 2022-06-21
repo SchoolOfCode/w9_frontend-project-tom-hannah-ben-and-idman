f6f6080ae6cd899faabdd6366cb30a9854ba062a
idman01 <idmman.abshir@gmail.com>
2022-06-21 14:35:07 +0100

    Plan completed

--- portfolio/src/components/App/index.js
+++ portfolio/src/components/App/index.js
@@ -22,3 +22,24 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+/* PLAN:
+- Structure of portfolio webpage: 
+Navbar- 
+Header - Header component: This will include brand name, slogan and background image
+Main - Components:
+1. Folio list component
+2. Folio image,  like, link and designer name 
+3. Sidebar component 
+4. Login component 
+5. Signup component 
+6. Upload component
+Footer - Footer component
+Button component 
+(Stretch goal components: Favourites section, User log in and log in upload)
+- Create test file for all components
