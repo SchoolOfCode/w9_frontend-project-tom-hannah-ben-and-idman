d10724f9e0d62ed04ae4c66e0ff98ed8795b924b
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-06-21 11:57:10 +0100

    initialise header layout and brand colours

--- portfolio/src/components/App/index.js
+++ portfolio/src/components/App/index.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img
+					src="/logos/portfol-logo-horiz-M.png"
+					className="App-logo"
+					alt="logo"
+				/>
+			</header>
+			<main>
+				<p>
+					Portfolios you can <em>actually</em> create.
+				</p>
+			</main>
+		</div>
+	);
 }
 
 export default App;
