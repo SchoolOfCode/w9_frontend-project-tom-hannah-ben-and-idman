1765d7397a9475e45efaf9583504d00ff284d883
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-06-22 11:21:19 +0100

    change img elements to background-images to cover the space

--- portfolio/src/components/Folio/index.js
+++ portfolio/src/components/Folio/index.js
@@ -1,15 +1,20 @@
 import "./folio.css";
 
 export default function Folio({ folio }) {
+	const bkImg = {
+		backgroundImage: `url(${folio.img})`,
+	};
+	const imgAlt = `Folio by ${folio.creator}`;
+
 	return (
 		<article className="folio">
 			<div className="folio-content">
-				<div className="folio-img-container">
-					<img
+				<div className="folio-img-container" style={bkImg} alt={imgAlt}>
+					{/* <img
 						className="folio-img"
-						src={folio.img}
+						src=""
 						alt={`Portfolio by ${folio.creator}`}
-					/>
+					/> */}
 				</div>
 				<div className="like-div">❤️</div>
 				<div className="folio-desc">
