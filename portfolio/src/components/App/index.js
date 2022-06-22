d7b198d4972fa13adce524083b36429127cd3bfe
BPGreenwood <53783989+B-P-Greenwood@users.noreply.github.com>
2022-06-22 09:44:38 +0100

    Removed open comment left from merge

--- portfolio/src/components/App/index.js
+++ portfolio/src/components/App/index.js
@@ -4,25 +4,19 @@ import Header from "../Header";
 import Footer from "../Footer";
 import FolioList from "../FolioList";
 
-
 function App() {
-	return (
-		<div className="App">
-			<Navbar />
-			<Header />
-			<main>
-				<p>
-				
-				</p>
-				<FolioList />
-			</main>
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <main>
+        <p></p>
+        <FolioList />
+      </main>
 
-	
-			<Footer />
-
-		</div>
-		
-	);
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
@@ -47,5 +41,3 @@ Create test file for all components
 
 (Stretch goal components: Favourites section, User log in and log in upload)
 - Create test file for all components*/
-
-*/
