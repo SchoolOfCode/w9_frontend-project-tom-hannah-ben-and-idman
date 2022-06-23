6259299c10d17947aff8613ef3bb7502d3f18044
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-06-23 18:09:57 +0100

    style error msg

--- portfolio/src/components/FolioList/index.js
+++ portfolio/src/components/FolioList/index.js
@@ -4,18 +4,22 @@ import { useState } from "react";
 import Folio from "../Folio";
 
 export default function FolioList({ portfolio }) {
-  if (typeof portfolio !== "undefined") {
-    if (portfolio.length === 0) {
-      return <p>Sorry, no portfolios found with that search.</p>;
-    }
-    return (
-      <section className="FolioList">
-        {portfolio.map((f) => {
-          return <Folio key={f.portfolio_id} folio={f} />;
-        })}
-      </section>
-    );
-  } else {
-    return <div> not working</div>;
-  }
+	if (typeof portfolio !== "undefined") {
+		if (portfolio.length === 0) {
+			return (
+				<p id="length-zero-msg">
+					Sorry, no portfolios found with that search.
+				</p>
+			);
+		}
+		return (
+			<section className="FolioList">
+				{portfolio.map((f) => {
+					return <Folio key={f.portfolio_id} folio={f} />;
+				})}
+			</section>
+		);
+	} else {
+		return <div> not working</div>;
+	}
 }
