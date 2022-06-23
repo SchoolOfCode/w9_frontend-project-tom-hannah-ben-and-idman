58ec833d8ff5182c3f04ec7f6589c6a6efd1aef6
BPGreenwood <53783989+B-P-Greenwood@users.noreply.github.com>
2022-06-23 17:11:49 +0100

    Updated experience search to remove console error

--- portfolio/src/components/ExperienceSearch/index.js
+++ portfolio/src/components/ExperienceSearch/index.js
@@ -1,18 +1,24 @@
-
 import "./experienceSearch.css";
 
-export default function ExperienceSearch({onChange}) {
+export default function ExperienceSearch({ onChange }) {
   return (
     <div>
-        <form className="experience-search-form">
+      <form className="experience-search-form">
         <label>Search by designer experience level </label>
-        <select name="searchInput" className="experience-search" onChange={onChange}>
-          <option value="initial" selected disabled hidden>Select option</option>
+        <select
+          name="searchInput"
+          className="experience-search"
+          onChange={onChange}
+          defaultValue="initial"
+        >
+          <option value="initial" disabled hidden>
+            Select option
+          </option>
           <option value="Beginner">Beginner</option>
           <option value="Mid-level">Mid-level</option>
           <option value="Experienced">Experienced</option>
         </select>
-        </form>
+      </form>
     </div>
   );
 }
