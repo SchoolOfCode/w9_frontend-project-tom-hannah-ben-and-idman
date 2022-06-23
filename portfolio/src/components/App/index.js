e214d89a62af75be385d247130b44eeecf098624
hcmcf <hannah@hannahmcfarlane.co.uk>
2022-06-23 16:54:07 +0100

    upload form padding

--- portfolio/src/components/App/index.js
+++ portfolio/src/components/App/index.js
@@ -10,108 +10,111 @@ import MenuContainer from "../MenuContainer";
 import ExperienceSearch from "../ExperienceSearch";
 
 function App() {
+	const [portfolio, setPortfolio] = useState([]);
 
-  const [portfolio, setPortfolio] = useState([]);
+	async function getData() {
+		let res = await fetch("http://localhost:3001/portfolio");
+		let data = await res.json();
+		console.log(data);
+		setPortfolio(data.payload);
+	}
 
-  async function getData() {
-    let res = await fetch("http://localhost:3001/portfolio");
-    let data = await res.json();
-    console.log(data);
-    setPortfolio(data.payload);
-  }
+	useEffect(() => {
+		getData();
+	}, []);
 
-  useEffect(() => {
-    getData();
-  }, []);
+	const [resetClass, setResetClass] = useState("reset-button");
+	//when keyword search done by user new api call completed
+	async function searchByKeyword(e) {
+		e.preventDefault();
+		const input = e.target.searchInput.value;
+		e.target.reset();
 
-  const [resetClass, setResetClass] = useState("reset-button");
-  //when keyword search done by user new api call completed
-  async function searchByKeyword(e) {
-    e.preventDefault();
-    const input = e.target.searchInput.value;
-    e.target.reset();
+		let res = await fetch(
+			`http://localhost:3001/portfolio/?keyword=${input}`
+		);
+		let data = await res.json();
+		setPortfolio(data.payload);
+		setResetClass("reset-button show");
+	}
 
-    let res = await fetch(`http://localhost:3001/portfolio/?keyword=${input}`);
-    let data = await res.json();
-    setPortfolio(data.payload);
-    setResetClass("reset-button show");
-  }
+	//when designer search done by user new api call completed
+	async function searchByDesigner(e) {
+		e.preventDefault();
+		const input = e.target.searchInput.value;
+		e.target.reset();
 
-  //when designer search done by user new api call completed
-  async function searchByDesigner(e) {
-    e.preventDefault();
-    const input = e.target.searchInput.value;
-    e.target.reset();
+		let res = await fetch(
+			`http://localhost:3001/portfolio/?designer=${input}`
+		);
+		let data = await res.json();
+		setPortfolio(data.payload);
+		setResetClass("reset-button show");
+	}
 
-    let res = await fetch(`http://localhost:3001/portfolio/?designer=${input}`);
-    let data = await res.json();
-    setPortfolio(data.payload);
-    setResetClass("reset-button show");
-  }
+	//when experience search done by user new api call completed
+	async function searchByExperience(e) {
+		e.preventDefault();
+		const input = e.target.value;
+		let res = await fetch(
+			`http://localhost:3001/portfolio/?experience=${input}`
+		);
+		let data = await res.json();
+		setPortfolio(data.payload);
+		setResetClass("reset-button show");
+	}
 
-  //when experience search done by user new api call completed
-  async function searchByExperience(e) {
-    e.preventDefault();
-    const input = e.target.value;
-    let res = await fetch(
-      `http://localhost:3001/portfolio/?experience=${input}`
-    );
-    let data = await res.json();
-    setPortfolio(data.payload);
-    setResetClass("reset-button show");
-  }
+	function onClick(e) {
+		e.preventDefault();
+		setResetClass("reset-button");
+		getData();
+	}
 
-  function onClick(e) {
-    e.preventDefault();
-    setResetClass("reset-button");
-    getData();
-  }
+	// VISIBILITY OF SLIDE OUT MENU
+	const [menuVis, setMenuVis] = useState({ visible: false });
 
-  // VISIBILITY OF SLIDE OUT MENU
-  const [menuVis, setMenuVis] = useState({ visible: false });
+	function toggleMenu() {
+		console.log("toggle run");
+		setMenuVis({ visible: !menuVis.visible });
+	}
 
-  function toggleMenu() {
-    console.log("toggle run");
-    setMenuVis({ visible: !menuVis.visible });
-  }
+	function menuOnClick() {
+		toggleMenu();
+		console.log("clicked");
+	}
 
-  function menuOnClick() {
-    toggleMenu();
-    console.log("clicked");
-  }
+	function resetSearch(e) {
+		e.preventDefault();
+		setResetClass("reset-button");
+		const help = document.querySelector(".experience-search");
+		help.value = "initial";
+		getData();
+	}
 
-  function resetSearch(e) {
-    e.preventDefault();
-    setResetClass("reset-button");
-    const help = document.querySelector(".experience-search");
-    help.value = "initial";
-    getData();
-  }
+	return (
+		<div className="App">
+			<Navbar menuOnClick={menuOnClick} />
+			<MenuContainer menuVis={menuVis} closeMenu={menuOnClick} />
+			<Header />
+			<div className="search-section">
+				<Search onSubmit={searchByKeyword} type="keyword" />
+				<Search onSubmit={searchByDesigner} type="Designer's Name" />
+				<ExperienceSearch onChange={searchByExperience} />
+				<button className={resetClass} onClick={resetSearch}>
+					Press to remove filter
+				</button>
+			</div>
 
-  return (
-    <div className="App">
-      <Navbar menuOnClick={menuOnClick} />
-      <MenuContainer menuVis={menuVis} closeMenu={menuOnClick} />
-      <Header />
-      <div className="search-section">
-        <Search onSubmit={searchByKeyword} type="keyword" />
-        <Search onSubmit={searchByDesigner} type="Designer's Name" />
-        <ExperienceSearch onChange={searchByExperience} />
-        <button className={resetClass} onClick={resetSearch}>
-          Press to remove filter
-        </button>
-      </div>
+			<main>
+				<p></p>
+				<FolioList portfolio={portfolio} />
+			</main>
 
-      <main>
-        <p></p>
-        <FolioList portfolio={portfolio} />
-      </main>
+			{/* <Upload /> */}
 
-      <Upload />
-
-      <Footer />
-    </div>
-  );
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
