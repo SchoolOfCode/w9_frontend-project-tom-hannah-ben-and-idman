c9b9aa2eeb2c37e5db95b68c23e988135231c3cb
TomBirbeck <tombirbeck47@gmail.com>
2022-06-28 16:46:10 +0100

    added links to authors

--- portfolio/README.md
+++ portfolio/README.md
@@ -89,10 +89,10 @@ Takes in the prop of 'upload' from the App index.js, passed down through the Men
 
 ## Authors
 
-- Ben Greenwood
-- Idman Abshir
-- Hannah McFarlane
-- Tom Birbeck
+- [Ben Greenwood](https://github.com/B-P-Greenwood)
+- [Idman Abshir](https://github.com/idman01)
+- [Hannah McFarlane](https://twitter.com/hannahdoesacode)
+- [Tom Birbeck](https://github.com/TomBirbeck)
 
 # Getting Started with Create React App
 
---
+++ portfolio/src/components/Search/search.test.js
@@ -0,0 +1,22 @@
+import { test, expect, jest } from "@jest/globals";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Search from "./index.js";
+import fetchMock from 'fetch-mock';
+import App from "../App"
+
+test(`search bar`, () => {
+    const onChange = jest.fn()
+    const type = "keyword"
+    render(<Search type = {type} onSubmit = {onChange} />);
+    expect(screen.getByLabelText(`Search by keyword:`)).toBeInTheDocument();
+    });
+
+test.only('search field', async () => {
+    const input = "animation";
+    const onSubmit = jest.fn();
+    fetchMock.mock(`http://localhost:3001/portfolio/?designer=animation`, 200);
+    var {getByTestId, findByTestId} = render(<App />)
+  fireEvent.click(getByTestId("apiCall"))
+  expect(await findByTestId("ptag")).toHaveTextContent('response');
+}) 
