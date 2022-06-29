435987801c24d09c97db75d171428069afbc0eff
TomBirbeck <tombirbeck47@gmail.com>
2022-06-29 10:31:28 +0100

    added test for search bar button

--- portfolio/src/components/Search/search.test.js
+++ portfolio/src/components/Search/search.test.js
@@ -2,8 +2,8 @@ import { test, expect, jest } from "@jest/globals";
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Search from "./index.js";
-import fetchMock from 'fetch-mock';
-import App from "../App"
+//import fetchMock from 'fetch-mock';
+//import App from "../App"
 
 test(`search bar`, () => {
     const onChange = jest.fn()
@@ -12,11 +12,20 @@ test(`search bar`, () => {
     expect(screen.getByLabelText(`Search by keyword:`)).toBeInTheDocument();
     });
 
-test.only('search field', async () => {
-    const input = "animation";
-    const onSubmit = jest.fn();
-    fetchMock.mock(`http://localhost:3001/portfolio/?designer=animation`, 200);
-    var {getByTestId, findByTestId} = render(<App />)
-  fireEvent.click(getByTestId("apiCall"))
-  expect(await findByTestId("ptag")).toHaveTextContent('response');
-}) 
+
+test(`search bar button`, () => {
+    
+    render(<Search />);
+    expect(screen.getByRole(`button`)).toBeInTheDocument();
+    });
+
+
+
+// test.only('search field', async () => {
+//     const input = "animation";
+//     const onSubmit = jest.fn();
+//     fetchMock.mock(`http://localhost:3001/portfolio/?designer=animation`, 200);
+//     var {getByTestId, findByTestId} = render(<App />)
+//   fireEvent.click(getByTestId("apiCall"))
+//   expect(await findByTestId("ptag")).toHaveTextContent('response');
+// }) 
